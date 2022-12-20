9dc29994b1728b7a7f01675338911e491068c254
Jordan Latter <Jlatter1111@gmail.com>
2022-12-20 18:55:38 +0000

    Add table and center it, lay out the basic comments of how each function will work

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// create 3x3 grid of divs
+// function an array to make the game board
+const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+const createboard = () => {
+  const gameboard = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+};
+
+// factory for making players
+const playerfactory = (name, naughtORcross) => {
+
+};
+// function to create a board that corresponds with the gameboard array
+// add eventlisteners to the divs and change the inner text to either O or X depending on the turn
+// add an AI component
+// add a round counter
