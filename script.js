c2e96a9bc9d24d5a3f35033b05ec86f6c1a9f1ae
Jordan Latter <Jlatter1111@gmail.com>
2022-12-21 18:19:46 +0000

    add form with eventlistener to get the player names then add the names to a player factory which will determine which player is "O" or "X"

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const form = document.getElementById('form');
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+// function to add player names to divs
+function playernames() {
+  const player1Id = document.getElementById('player1-id').value;
+  const player2Id = document.getElementById('player2-id').value;
+
+  player1.innerHTML = player1Id;
+  player2.innerHTML = player2Id;
+}
+
+// eventlistener for form
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  playernames();
+});
+
 // function an array to make the game board
 const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
   [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
@@ -7,7 +25,11 @@ const createboard = () => {
 
 // factory for making players
 const playerfactory = (name, naughtORcross) => {
-
+const playerReal1.name = player1.innerHTML
+const playerReal1.naughtORcross = "O"
+const playerReal2.name = player2.innerHTML
+const playerReal2.naughtORcross = "X"
+const 
 };
 // function to create a board that corresponds with the gameboard array
 // add eventlisteners to the divs and change the inner text to either O or X depending on the turn
