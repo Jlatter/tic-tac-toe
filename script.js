459f189b7f1c33b89d975162801ea9bcaf667c6b
Jordan Latter <Jlatter1111@gmail.com>
2023-01-06 16:59:57 +0000

    add functions to check win status and draw status which displays a message and resets the board, add a computer function which randomly selects a card when its the "X" turn, add styling to the page, add round counter and win counter for each player

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
+/* eslint-disable no-undef */
 const form = document.getElementById('form');
 const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
 const cards = document.querySelectorAll('.card');
+const winMessage = document.getElementById('winner');
+const container = document.getElementById('container');
+const formCon = document.getElementById('formContainer');
+const play1score = document.getElementById('p1score');
+const play2score = document.getElementById('p2score');
+const round = document.getElementById('round');
+const aiCheckbox = document.getElementById('ai');
+const availableNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
+// eslint-disable-next-line prefer-const
+let isChecked = aiCheckbox.checked;
+
+play1score.innerHTML = parseFloat(0);
+play2score.innerHTML = parseFloat(0);
+round.innerHTML += parseFloat(1);
+let num = 0;
+let sum = 0;
+let roundNum = 1;
+
+container.classList.add('target');
 // factory for making players
 const playerfactory = (name, naughtOrcross) => ({ name, naughtOrcross });
-
+let playchar1 = playerfactory('non', 'O');
+let playchar2 = playerfactory('nons', 'X');
 // function to add player names to divs
 function playernames() {
   const player1Id = document.getElementById('player1-id').value;
   const player2Id = document.getElementById('player2-id').value;
-  const playchar1 = playerfactory(player1Id, 'O');
-  const playchar2 = playerfactory(player2Id, 'X');
-
+  playchar1 = playerfactory(player1Id, 'O');
+  playchar2 = playerfactory(player2Id, 'X');
   player1.textContent = playchar1.name;
-  console.log(playchar1.name);
 
   player2.textContent = playchar2.name;
   return { playchar1, playchar2 };
 }
-const { playchar1, playchar2 } = playernames();
+
+let currentPlayer = playchar1;
 // function and array to make the game board
 const win = [
   [0, 1, 2],
@@ -39,28 +59,95 @@ const createboard = () => {
 // eventlistener for form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
+  formCon.classList.add('target');
+  container.classList.remove('target');
+  isChecked = aiCheckbox.checked;
   playernames();
   createboard();
 });
-let currentPlayer = playchar1;
+
+function checkwin() {
+  for (const comb of win) {
+    if (
+      comb.every(
+        (index) => cards[index].textContent === currentPlayer.naughtOrcross
+      )
+    ) {
+      if (currentPlayer.naughtOrcross === 'O') {
+        num = parseInt(num) + 1;
+        play1score.innerHTML = num;
+        roundNum = parseInt(roundNum) + 1;
+        round.innerHTML = `Round ${roundNum}`;
+
+        console.log(currentPlayer.naughtOrcross);
+      } else {
+        sum = parseInt(sum) + 1;
+        play2score.innerHTML = sum;
+        roundNum = parseInt(roundNum) + 1;
+        round.innerHTML = `Round ${roundNum}`;
+      }
+      winMessage.innerHTML = `${currentPlayer.name} wins`;
+      setTimeout(() => {
+        cards.forEach((card) => {
+          card.innerHTML = '';
+          winMessage.innerHTML = '';
+        });
+      }, 2500);
+    }
+  }
+}
+function checkDraw() {
+  if (Array.prototype.every.call(cards, (card) => card.textContent !== '')) {
+    winMessage.innerHTML = 'Draw!';
+    round.innerHTML = `Round ${roundNum}`;
+    setTimeout(() => {
+      cards.forEach((card) => {
+        card.innerHTML = '';
+        winMessage.innerHTML = '';
+      });
+    }, 2500);
+  }
+}
+
+function computerMove() {
+  // Generate a random number between 0 and 8
+  let randomIndex = Math.floor(Math.random() * 9);
+
+  // Select the square at the random index
+  let square = cards[randomIndex];
+
+  // Keep generating a new random index until an empty square is found
+  while (square.textContent !== '') {
+    randomIndex = Math.floor(Math.random() * 9);
+    square = cards[randomIndex];
+  }
+
+  // Place an "X" in the empty square
+  square.textContent = 'X';
+  checkwin();
+  checkDraw();
+}
+
 cards.forEach((card) => {
-  card.addEventListener('click', (event) => {
+  card.addEventListener('click', () => {
     if (card.textContent !== '') {
       return;
     }
     card.innerHTML = currentPlayer.naughtOrcross;
-    console.log(currentPlayer.naughtOrcross);
+    checkwin();
+    checkDraw();
 
-    if (currentPlayer === playchar1) {
+    if (currentPlayer.naughtOrcross === 'O') {
       currentPlayer = playchar2;
     } else {
       currentPlayer = playchar1;
     }
+    if (isChecked === true) {
+      computerMove();
+      currentPlayer = playchar1;
+    }
   });
 });
-
-// function to create a board that corresponds with the gameboard array
+// If there are any available numbers, choose one at random
 
 // add an AI component
-// add a round counter
